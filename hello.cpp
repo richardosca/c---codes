45d99a1357cf16bb80af999b60459fb29d910685
richardosca <richardsonreuben78@gmail.com>
2023-06-09 15:19:48 +0300

    first commit

---
+++ hello.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int a;
+
+int main()
+{
+    int a=3;
+
+    int sum;
+    sum=a+b;
+    cout << "the sum is =" << sum;
+    
+    /* code */
+    return 0;
+}
---
+++ simple.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+     int number;
+     cout<<"enter the value";
+cin>> number;
+cout << "the entered input is="<< number;
+   
+
+    /* code */
+    return 0;
+}
